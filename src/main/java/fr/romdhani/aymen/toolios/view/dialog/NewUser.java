e045ed9908fba584b49a493b4f4069522363e7e6
aymen <ayman.romdhani@gmail.com>
2020-07-02 22:10:26 +0200

    [toolios-quality] dao changes

---
+++ src/main/java/fr/romdhani/aymen/toolios/view/dialog/NewUser.java
@@ -0,0 +1,17 @@
+package fr.romdhani.aymen.toolios.view.dialog;
+
+import javax.swing.*;
+
+public class NewUser extends JDialog {
+    private void initComponents() {
+        JPanel userPanel = new JPanel();
+        JLabel fName = new JLabel();
+        JTextField fNameTextField = new JTextField();
+        JLabel lName = new JLabel();
+        JTextField lNameTextField = new JTextField();
+        JLabel loginLabel = new JLabel();
+        JTextField loginTextField = new JTextField();
+        JLabel emailLabel = new JLabel();
+        JTextField emailTextField = new JTextField();
+    }
+}
