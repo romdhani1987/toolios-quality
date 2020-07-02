99e9acff3ba6c51a410dc5fd8afe749180fed0a5
aymen <ayman.romdhani@gmail.com>
2020-07-02 21:14:17 +0200

    [toolios-quality] dao changes

--- src/main/java/fr/romdhani/aymen/toolios/view/menu/TooliosMenubar.java
+++ src/main/java/fr/romdhani/aymen/toolios/view/menu/TooliosMenubar.java
@@ -23,12 +23,24 @@ public class TooliosMenubar extends JMenuBar {
 
     public void initComponents() {
         JMenu menuFile = new JMenu("File");
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        menuFile.add(openMenuItem);
+        JMenuItem closeMenuItem = new JMenuItem("Exit");
+        menuFile.add(closeMenuItem);
+        JMenuItem exportMenuItem = new JMenuItem("Export");
+        menuFile.add(exportMenuItem);
+        JMenuItem saveAsMenuItem = new JMenuItem("Save as");
+        menuFile.add(saveAsMenuItem);
         listMenu.add(menuFile);
         JMenu menuConnection = new JMenu("Connection");
         listMenu.add(menuConnection);
         JMenu settingsFile = new JMenu("Settings");
+        JMenuItem dbMenuItem = new JMenuItem("Database settings");
+        settingsFile.add(dbMenuItem);
         listMenu.add(settingsFile);
         JMenu menuHelp = new JMenu("Help");
+        JMenuItem helpMenuItem = new JMenuItem("?");
+        menuHelp.add(helpMenuItem);
         listMenu.add(menuHelp);
     }
 
