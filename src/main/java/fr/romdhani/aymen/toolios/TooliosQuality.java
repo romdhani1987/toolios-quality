b97502e78fac8d3cc94426544c9d6662098a7666
aymen <ayman.romdhani@gmail.com>
2020-07-05 21:10:10 +0200

    [toolios-quality] edit user dialog

--- src/main/java/fr/romdhani/aymen/toolios/TooliosQuality.java
+++ src/main/java/fr/romdhani/aymen/toolios/TooliosQuality.java
@@ -8,28 +8,14 @@ import java.awt.*;
  * Class used to perform CRUD operation on database with Hibernate API's
  */
 public class TooliosQuality {
-
     @SuppressWarnings("unused")
     public static void main(String[] args) {
-      /*
-        UserAccountService userAccountService = new UserAccountService();
-        UserAccount book2 = new UserAccount("aymenromd", "romdhani");
-        UserAccount book3 = new UserAccount("sarah", "romdhani");
-        System.out.println("*** Persist - start ***");
-        userAccountService.persist(book2);
-        userAccountService.persist(book3);
-        List<UserAccount> userAccountList = userAccountService.findAll();
-        System.out.println("User Persisted are :");
-        for (UserAccount userAccount : userAccountList) {
-            System.out.println("-" + userAccount.getLogin());
-        }
-         System.out.println("*** Persist - end ***");
-        */
         System.out.println("*** Start Toolios-Quality ***");
         TooliosQualityView tooliosQualityView = new TooliosQualityView("Toolios-Quality");
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         tooliosQualityView.setPreferredSize(new Dimension(1024, 800));
         tooliosQualityView.pack();
+        tooliosQualityView.setLocationRelativeTo(null);
         tooliosQualityView.setVisible(true);
     }
 
