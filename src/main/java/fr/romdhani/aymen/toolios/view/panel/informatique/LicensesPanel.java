a4c481c6490e93281fb8721ef9b14d32f9330b7a
aymen <ayman.romdhani@gmail.com>
2020-07-10 23:57:38 +0200

    [toolios-quality] edit user dialog

---
+++ src/main/java/fr/romdhani/aymen/toolios/view/panel/informatique/LicensesPanel.java
@@ -0,0 +1,47 @@
+package fr.romdhani.aymen.toolios.view.panel.informatique;
+
+import fr.romdhani.aymen.toolios.controller.ScreenController;
+import fr.romdhani.aymen.toolios.controller.user.UserController;
+import fr.romdhani.aymen.toolios.view.buttons.TooliosButton;
+import fr.romdhani.aymen.toolios.view.panel.TooliosView;
+import fr.romdhani.aymen.toolios.view.table.model.ScreenModelObject;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LicensesPanel extends JPanel implements TooliosView {
+
+    private ScreenController screenController;
+
+    public LicensesPanel(ScreenController screenController) {
+        super();
+        this.screenController = screenController;
+        initComponents();
+    }
+
+    private JTable screensTable;
+    private ScreenModelObject userModelObject;
+    private TooliosButton addButton;
+    private TooliosButton editButton;
+    private TooliosButton removeButton;
+    private TooliosButton updateButton;
+    private UserController userController;
+
+    private void initComponents() {
+        setLayout(new MigLayout());
+        userModelObject = new ScreenModelObject();
+        screensTable = new JTable(userModelObject);
+        addButton = new TooliosButton("Add");
+        editButton = new TooliosButton("Edit");
+        removeButton = new TooliosButton("Delete");
+        updateButton = new TooliosButton("Refresh");
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonsPanel.add(addButton);
+        buttonsPanel.add(editButton);
+        buttonsPanel.add(removeButton);
+        buttonsPanel.add(updateButton);
+        this.add(new JScrollPane(screensTable), "grow,span, push");
+        this.add(buttonsPanel, "grow,span,push");
+    }
+}
